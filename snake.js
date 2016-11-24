d17b764d9962dfdbee5274b71ed60fe3127fc1b5
Merksue <gabeaboy@gmail.com>
2016-11-24 16:40:54 -0500

    Canvas ready

---
+++ snake.js
@@ -0,0 +1,11 @@
+//canvas stuff
+var canvas = $('#canvas')[0];
+var ctx = canvas.getContext('2d');
+var w = $('#canvas').width();
+var h = $('#canvas').height();
+
+//paint
+ctx.fillStyle = "white";
+ctx.fillRect(0,0,w,h);
+ctx.strokeStyle = 'black';
+ctx.strokeRect(0,0,w,h);

15f5bf2ead89e9a495c4691f749c9108efe79aa2
Merksue <gabeaboy@gmail.com>
2016-11-25 16:38:16 -0500

    day 1 - Movement

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # snake
+#November 25 2016
+*TO-ACCOMPLISH
+1) Mobility
+      a. Make buttons with key strokes wasd to move the x y coordinates by 1
+      b. Set boundaries to stop infinity grid end game on those points (collision)
+      c. Auto walk capability
+2) Gluttony for the snake
+      Random food generator
+      Growing the progress
+3) AmIthere yet?
+    var score = snakeArray.length;*
+
+#1.a
+Functions to look at paint() and snakeHatchery()
+Need to change x and y over time with a setInterval(paint,60ms);
+make que pop and replace on other end
+*Finished, infinite slither *
+#boundaries needed, mobility 
--- snake.js
+++ snake.js
@@ -6,20 +6,23 @@ $(document).ready(function(){
   var ctx = canvas.getContext('2d');
   var w = $('#canvas').width();
   var h = $('#canvas').height();
-
+  var cw = 10;//cell width
   //paint
   //fill-area stroke-perimeter
   //style-color rect-thing
-  ctx.fillStyle = "white";
-  ctx.fillRect(0,0,w,h);
-  ctx.strokeStyle = 'black';
-  ctx.strokeRect(0,0,w,h);
-
-
+  // x y width height
+  // ctx.fillStyle = "white";
+  // ctx.fillRect(0,0,w,h);//x y width height
+  // ctx.strokeStyle = 'black';//y goes down
+  // ctx.strokeRect(0,0,w,h);
   //
+  // //
   var snakeArray;
   snakeHatchery();
+  //1 make
   function snakeHatchery() {
+    //pushing an object for each cell giving it
+    //its own x and y coordinate
     var length = 5;
     snakeArray = [];
     for (var i = length-1; i >= 0; i--) {
@@ -27,15 +30,30 @@ $(document).ready(function(){
     }
   }
   function paint() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0,w,h);//x y width height
+    ctx.strokeStyle = 'black';//y goes down
+    ctx.strokeRect(0,0,w,h);
+
+    var nx = snakeArray[0].x;
+    var ny = snakeArray[0].y;
+    nx++;
+    var tail = snakeArray.pop();
+    tail.x = nx;
+    snakeArray.unshift(tail);
+
     for (var i = 0; i < snakeArray.length; i++) {
       var c = snakeArray[i];
       //size of snakeArray cells
       ctx.fillStyle='blue';
-      ctx.fillRect(c.x*10,c.y*10,10,10);
+      ctx.fillRect(c.x*cw,c.y*cw,cw,cw);
       ctx.strokeStyle = 'white';
-      ctx.strokeRect(c.x*10,c.y*10,10,10);
+      ctx.strokeRect(c.x*cw,c.y*cw,cw,cw);
     }
   }
   paint();
+
+game_loop = setInterval(paint,1000);
+
 
 });

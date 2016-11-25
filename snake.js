57c5eddb8665711c8d0da1a4913801e4e2880c92
Merksue <gabeaboy@gmail.com>
2016-11-24 19:17:42 -0500

    still picture of snake

--- snake.js
+++ snake.js
@@ -1,11 +1,41 @@
 //canvas stuff
-var canvas = $('#canvas')[0];
-var ctx = canvas.getContext('2d');
-var w = $('#canvas').width();
-var h = $('#canvas').height();
+$(document).ready(function(){
 
-//paint
-ctx.fillStyle = "white";
-ctx.fillRect(0,0,w,h);
-ctx.strokeStyle = 'black';
-ctx.strokeRect(0,0,w,h);
+
+  var canvas = $('#canvas')[0];
+  var ctx = canvas.getContext('2d');
+  var w = $('#canvas').width();
+  var h = $('#canvas').height();
+
+  //paint
+  //fill-area stroke-perimeter
+  //style-color rect-thing
+  ctx.fillStyle = "white";
+  ctx.fillRect(0,0,w,h);
+  ctx.strokeStyle = 'black';
+  ctx.strokeRect(0,0,w,h);
+
+
+  //
+  var snakeArray;
+  snakeHatchery();
+  function snakeHatchery() {
+    var length = 5;
+    snakeArray = [];
+    for (var i = length-1; i >= 0; i--) {
+      snakeArray.push({x:i,y:0});
+    }
+  }
+  function paint() {
+    for (var i = 0; i < snakeArray.length; i++) {
+      var c = snakeArray[i];
+      //size of snakeArray cells
+      ctx.fillStyle='blue';
+      ctx.fillRect(c.x*10,c.y*10,10,10);
+      ctx.strokeStyle = 'white';
+      ctx.strokeRect(c.x*10,c.y*10,10,10);
+    }
+  }
+  paint();
+
+});
